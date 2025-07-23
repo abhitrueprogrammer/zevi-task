61ff84331d70c7dbdb3f5d3b7ad6e6f3594332a2
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-07-24 03:57:21 +0530

    zevi build fix

--- src/app/data/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import data from "./data.json";
-
-const items = data.items as Item[];
-
-export function GET(req: NextRequest) {
-  return NextResponse.json({ items });
-}
